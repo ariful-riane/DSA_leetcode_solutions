f6b10b1039e6d2098f67a30ffefd89507b937f86
Ariful Riane <arifulrian@gmail.com>
2025-12-21 12:23:03 +0000

    [27] [Time Beats: 100.00%] [Memory Beats: 97.82%] - LeetPush

--- Remove-Element.py
+++ Remove-Element.py
@@ -1,7 +1,7 @@
-11#Program: Remove Element
-22#Author: Ariful Riane
-33#Input: a list of integars and a value
-44#Output: return the remaining length of the list after removing the value
+1#Program: Remove Element
+2#Author: Ariful Riane
+3#Input: a list of integars and a value
+4#Output: return the remaining length of the list after removing the value
 5class Solution:
 6    def removeElement(self, nums: List[int], val: int) -> int:
 7        i = 0
